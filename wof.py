aecc96c17466a17547780ee9a078a32044c2dc96
RaymondBernard <ray.bernard@outlook.com>
2023-07-19 18:25:31 -0400

    add llms names

--- wof.py
+++ wof.py
@@ -1,127 +1,108 @@
 import random
 
-# The Wheel of Fortune wheel
-WHEEL = [{'type': 'cash', 'value': 5000, 'text': '$5000'},
+# Define the wheel and other game components
+WHEEL = [{'type': 'cash', 'value': 500, 'text': '$500'},
+         {'type': 'cash', 'value': 1000, 'text': '$1000'},
          {'type': 'bankrupt', 'value': 0, 'text': 'Bankrupt'},
-         {'type': 'cash', 'value': 600, 'text': '$600'},
-         {'type': 'lose_a_turn', 'value': 0, 'text': 'Lose a turn'},
-         {'type': 'cash', 'value': 700, 'text': '$700'},
-         {'type': 'cash', 'value': 900, 'text': '$900'},
-         {'type': 'cash', 'value': 300, 'text': '$300'},
-         {'type': 'cash', 'value': 800, 'text': '$800'},
-         {'type': 'cash', 'value': 550, 'text': '$550'},
-         {'type': 'cash', 'value': 400, 'text': '$400'},
-         {'type': 'cash', 'value': 500, 'text': '$500'},
-         {'type': 'cash', 'value': 450, 'text': '$450'}]
+         {'type': 'lose_a_turn', 'value': 0, 'text': 'Lose a Turn'}]
 
-# The categories of phrases
-CATEGORIES = ['Around The House', 'Food & Drink', 'Things', 'Living Things', 'Place']
+CATEGORIES = ['Phrase', 'Person', 'Place', 'Thing', 'Title']
+PHRASES = {'Phrase': ['A CHIP OFF THE OLD BLOCK', 'A FISH OUT OF WATER', 'A LEOPARD CANNOT CHANGE ITS SPOTS'],
+           'Person': ['JOHN DOE', 'JANE DOE'],
+           'Place': ['NEW YORK', 'PARIS'],
+           'Thing': ['WHEEL OF FORTUNE', 'DICTIONARY'],
+           'Title': ['THE GREAT GATSBY', 'BRAVE NEW WORLD']}
+VOWELS = 'AEIOU'
+CONSONANTS = 'BCDFGHJKLMNPQRSTVWXYZ'
+VOWEL_COST = 250
 
-# The phrases to guess
-PHRASES = {'Around The House': ['ironing board', 'washing machine', 'dining table'],
-           'Food & Drink': ['banana smoothie', 'roasted turkey', 'creamy pasta'],
-           'Things': ['mirror', 'chair', 'pencil', 'notebook'],
-           'Living Things': ['elephant', 'puppy', 'sparrow', 'human'],
-           'Place': ['New York', 'India', 'Buckingham Palace', 'Mount Everest']}
+# The player class
+class Player:
+    def __init__(self, name):
+        self.name = name
+        self.score = 0
 
-# Players
-PLAYERS = ['Human player #1', 'Human player #2', 'Human player #3']
-
+# Functions for the game logic
 def spinWheel():
-    """Return a random item from the wheel."""
     return random.choice(WHEEL)
 
 def getRandomCategoryAndPhrase():
-    """Get a random category and phrase from PHRASES."""
     category = random.choice(CATEGORIES)
     phrase = random.choice(PHRASES[category])
-    return category, phrase.lower()
+    return category, phrase
 
 def obscurePhrase(phrase, guessed):
-    """Obscure each letter in the phrase that has not been guessed."""
-    return ''.join('_ ' if letter != ' ' and letter not in guessed else letter for letter in phrase)
+    return ''.join('_ ' if letter not in guessed else letter for letter in phrase)
 
-def showBoard(category, obscuredPhrase, guessed):
-    """Show the category, obscured phrase, and letters guessed."""
-    print('Category: ', category)
-    print('Phrase: ', obscuredPhrase)
-    print('Guessed: ', ', '.join(sorted(guessed)))
-
-def getPlayerMove(player, category, obscuredPhrase, guessed):
-    """Get a player's move."""
-    print(player, "it's your turn!")
+def getGuess():
     while True:
-        print("What do you want to do?")
-        print("  1) Spin the wheel")
-        print("  2) Buy a vowel for $250")
-        print("  3) Solve the puzzle")
-        option = input("> ")
-        if option == '1':
-            return spinWheel()
-        elif option == '2':
-            return 'buy_vowel'
-        elif option == '3':
-            return 'solve_puzzle'
+        guess = input("Enter your guess: ").upper()
+        if len(guess) == 1 and guess in CONSONANTS:
+            return guess
         else:
-            print("Invalid option. Try again.")
+            print("Invalid guess. Please enter a consonant.")
 
-# Game loop
+def buyVowel(player):
+    while True:
+        vowel = input("Enter a vowel: ").upper()
+        if len(vowel) == 1 and vowel in VOWELS:
+            player.score -= VOWEL_COST
+            return vowel
+        else:
+            print("Invalid input. Please enter a vowel.")
+
+# The main game loop
 def main():
+    # Create the players
+    players = [Player('ChatGPTv4 1'), Player('Google Bert(aka Bard) 2'), Player('LaMMA')]
+
+    # Start the game
     print('Welcome to Wheel of Fortune!')
-    
     category, phrase = getRandomCategoryAndPhrase()
     guessed = set()
-    playerIndex = 0
-    roundOver = False
 
-    while not roundOver:
-        player = PLAYERS[playerIndex]
-        showBoard(category, obscurePhrase(phrase, guessed), guessed)
-        
-        move = getPlayerMove(player, category, obscurePhrase(phrase, guessed), guessed)
-        
-        if type(move) == dict:  # the player chose to spin the wheel
-            if move['type'] == 'cash':
-                print(player, "spins...")
-                print(move['text'] + '!')
-                guess = input("Guess a consonant: ").lower()
-                if guess in guessed:
-                    print("You've already guessed that letter.")
-                elif guess in phrase:
-                    print("Good guess!")
+    # Game rounds
+    playerIndex = 0
+    while True:
+        player = players[playerIndex]
+        print(f"It's {player.name}'s turn. You have {player.score} points.")
+        print(obscurePhrase(phrase, guessed))
+
+        action = input("What do you want to do? (1- Spin the wheel, 2- Buy a vowel, 3- Solve the puzzle): ")
+        if action == '1':
+            spin = spinWheel()
+            print("You spun: ", spin['text'])
+            if spin['type'] == 'bankrupt':
+                player.score = 0
+            elif spin['type'] == 'lose_a_turn':
+                pass
+            else:
+                guess = getGuess()
+                if guess in phrase:
+                    player.score += spin['value']
                     guessed.add(guess)
+                    print(f"Good guess! Your score is now {player.score}")
                 else:
                     print("Sorry, that letter is not in the phrase.")
-            elif move['type'] == 'bankrupt':
-                print(player, "spins...")
-                print(move['text'] + '!')
-                print("You've gone bankrupt. You lose your turn.")
-            elif move['type'] == 'lose_a_turn':
-                print(player, "spins...")
-                print(move['text'] + '!')
-                print("You lose your turn.")
-
-        elif move == 'buy_vowel':
-            print(player, "chooses to buy a vowel.")
-            guess = input("Guess a vowel: ").lower()
-            if guess in guessed:
-                print("You've already guessed that letter.")
-            elif guess in phrase:
-                print("Good guess!")
-                guessed.add(guess)
+        elif action == '2':
+            if player.score < VOWEL_COST:
+                print(f"You don't have enough points to buy a vowel. You currently have {player.score} points.")
             else:
-                print("Sorry, that letter is not in the phrase.")
-
-        elif move == 'solve_puzzle':
-            print(player, "chooses to solve the puzzle.")
-            guess = input("Guess the whole phrase: ").lower()
+                vowel = buyVowel(player)
+                if vowel in phrase:
+                    guessed.add(vowel)
+                    print("Good guess!")
+                else:
+                    print("Sorry, that letter is not in the phrase.")
+        elif action == '3':
+            guess = input("What's your solution? ").upper()
             if guess == phrase:
-                print("Congratulations! You've solved the puzzle!")
-                roundOver = True
+                print(f"Congratulations, {player.name}! You solved the puzzle!")
+                break
             else:
-                print("Sorry, that's not the correct phrase.")
+                print("Sorry, that's not correct.")
 
-        playerIndex = (playerIndex + 1) % len(PLAYERS)  # move on to the next player
+        playerIndex = (playerIndex + 1) % len(players)
 
 if __name__ == '__main__':
     main()
