94c5ed4f70623db9a54cab2225f7f859fbf2e0d0
RaymondBernard <ray.bernard@outlook.com>
2023-07-21 20:04:48 -0400

    remove tranformers.py and wofbard.py

--- transformers.py
+++
@@ -1,9 +0,0 @@
-import transformers
-
-model = transformers.BertForQuestionAnswering.from_pretrained("bert-base-uncased")
-
-question = "What is the capital of France?"
-
-answer = model.predict(question)
-
-print(answer)
